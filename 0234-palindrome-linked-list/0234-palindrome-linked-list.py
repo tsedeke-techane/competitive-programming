97b22195a71feb1b1b72370141694220a29e31c0
Tsedeke Techane <164669795+tsedeke-techane@users.noreply.github.com>
2025-02-21 18:19:28 +0300

    Time: 40 ms (38.62%), Space: 35 MB (52.35%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -3,39 +3,33 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         
-        # middle
+        # Step 1: Find the middle of the list using slow and fast pointers
         slow = fast = head
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-        middle = slow
-
-        # reverse
+        
+        # Step 2: Reverse the second half of the list
         prev = None
-        while middle:
-            temp =middle.next
-            middle.next = prev
-            prev = middle
-            middle = temp
-
+        while slow:
+            temp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = temp
+        
+        # prev now points to the head of the reversed second half
         reversed_head = prev
-
-        # compare
         original_head = head
-
-        while original_head and reversed_head:
-            if original_head.val == reversed_head.val:
-                original_head = original_head.next
-                reversed_head = reversed_head.next
-
-            else:
+        
+        # Step 3: Compare both halves
+        while reversed_head:  
+            if original_head.val != reversed_head.val:
                 return False
-
+            original_head = original_head.next
+            reversed_head = reversed_head.next
+        
         return True
-
-                
-        
-        
