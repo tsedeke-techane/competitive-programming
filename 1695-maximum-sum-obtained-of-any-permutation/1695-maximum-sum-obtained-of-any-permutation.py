fb3a102b2be164a49be256e1d0b6d519355cfb7e
Tsedeke Techane <164669795+tsedeke-techane@users.noreply.github.com>
2025-03-05 11:45:36 +0300

    Time: 79 ms (94.78%), Space: 44.9 MB (48.85%) - LeetHub

--- 1695-maximum-sum-obtained-of-any-permutation/1695-maximum-sum-obtained-of-any-permutation.py
+++ 1695-maximum-sum-obtained-of-any-permutation/1695-maximum-sum-obtained-of-any-permutation.py
@@ -1,20 +1,20 @@
 class Solution:
     def maxSumRangeQuery(self, nums: List[int], requests: List[List[int]]) -> int:
         n = len(nums)
-        freq = [0] * n
+        freq = [0] * (n + 1)
         ans = 0
         for start, end in requests:
             freq[start] += 1
-            if end + 1 < n:
-                freq[end + 1] -= 1
+            freq[end + 1] -= 1
         
-        for i in range(1, n):
+        for i in range(1, n + 1):
             freq[i] += freq[i - 1]
 
-        nums.sort()
-        freq.sort()
+        nums.sort(reverse=True)
+        freq.sort(reverse=True)
 
         max_total = sum(num * fr for num, fr in zip(nums, freq))
+
         ans = max_total % (10**9 + 7)
 
         return ans
